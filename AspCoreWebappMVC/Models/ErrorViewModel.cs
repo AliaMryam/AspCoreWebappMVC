2a4c6f76a6b7465735146492577b5b229435e01b
aliam <aliam@ALIA>
2024-10-09 13:57:18 +0500

    Add project files.

---
+++ AspCoreWebappMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspCoreWebappMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
